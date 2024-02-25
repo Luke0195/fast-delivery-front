3e65493c8a41b43b037acb5cd34e9e34ef4835cb
Lucas Santos <lucas201273@gmail.com>
2024-02-25 10:37:51 -0300

    style: add animation on signin page

--- src/app/(home)/components/Form/index.tsx
+++ src/app/(home)/components/Form/index.tsx
@@ -1,26 +1,52 @@
-import { Logo, InputRoot, ButtonRoot } from '@app/components'
+'use client'
+import { InputRoot, ButtonRoot } from '@app/components'
+import { motion } from 'framer-motion'
 
 export function Form() {
   return (
     <form className="flex flex-col items-center">
-      <InputRoot.InputWrapper>
-        <InputRoot.InputLabel content="E-mail" />
-        <InputRoot.Input placeholder="Informe o seu email" />
-      </InputRoot.InputWrapper>
-      <InputRoot.InputWrapper>
-        <InputRoot.InputLabel content="Senha" />
-        <InputRoot.Input placeholder="Informe o sua senha" type="password" />
-      </InputRoot.InputWrapper>
-      <span className="block mt-2 font-normal text-base text-white hover:underline  cursor-pointer">
+      <motion.div
+        initial={{ x: 180, opacity: 0.1 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 2 }}
+      >
+        <InputRoot.InputWrapper>
+          <InputRoot.InputLabel content="E-mail" />
+          <InputRoot.Input placeholder="Informe o seu email" />
+        </InputRoot.InputWrapper>
+      </motion.div>
+      <motion.div
+        initial={{ x: -180, opacity: 0.1 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 2 }}
+      >
+        <InputRoot.InputWrapper>
+          <InputRoot.InputLabel content="Senha" />
+          <InputRoot.Input placeholder="Informe o sua senha" type="password" />
+        </InputRoot.InputWrapper>
+      </motion.div>
+
+      <motion.span
+        className="block mt-2 font-normal text-base text-white hover:underline  cursor-pointer"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 5 }}
+      >
         {' '}
         Esqueci minha Senha
-      </span>
-      <ButtonRoot.Button
-        className="mt-4 bg-primaryYellow text-black cursor-pointer hover:opacity-90  "
-        style={{ width: 384 }}
+      </motion.span>
+      <motion.div
+        initial={{ y: 90, opacity: 0.1 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 2 }}
       >
-        <ButtonRoot.ButtonContent> Entrar </ButtonRoot.ButtonContent>
-      </ButtonRoot.Button>
+        <ButtonRoot.Button
+          className="mt-4 bg-primaryYellow text-black cursor-pointer hover:opacity-90  "
+          style={{ width: 384 }}
+        >
+          <ButtonRoot.ButtonContent> Entrar </ButtonRoot.ButtonContent>
+        </ButtonRoot.Button>
+      </motion.div>
     </form>
   )
 }
--- src/app/(home)/components/Info/index.tsx
+++ src/app/(home)/components/Info/index.tsx
@@ -1,17 +1,26 @@
+'use client'
 import { Logo } from '@app/components'
+import { motion } from 'framer-motion'
 
 export const Info = () => {
   return (
     <>
       <Logo />
-      <h2 className="my-2 text-white text-2xl font-bold italic">
-        <span className="text-primaryYellow font-semibold">Gerencie</span> suas
-        encomendas com o <span className="text-primaryYellow">Fast</span>
-        Feet
-      </h2>
-      <span className="text-white my-1">
-        Faça seu login para gerenciar suas encomendas.
-      </span>
+      <motion.div
+        className="flex flex-col items-center"
+        animate={{ opacity: 1 }}
+        transition={{ duration: 4 }}
+        initial={{ opacity: 0.1 }}
+      >
+        <h2 className="my-2 text-white text-2xl font-bold italic">
+          <span className="text-primaryYellow font-semibold">Gerencie</span>{' '}
+          suas encomendas com o <span className="text-primaryYellow">Fast</span>
+          Feet
+        </h2>
+        <span className="text-white my-1">
+          Faça seu login para gerenciar suas encomendas.
+        </span>
+      </motion.div>
     </>
   )
 }
