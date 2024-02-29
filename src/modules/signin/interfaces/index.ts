0b3b0539ff5dccc3606f2518aced37bc0bf20532
Lucas Santos <lucas201273@gmail.com>
2024-02-29 17:42:17 -0300

    test: should render SignIn page correctly

--- src/modules/signin/interfaces/index.ts
+++ src/modules/signin/interfaces/index.ts
@@ -2,3 +2,7 @@ export type AuthenticationParams = {
   email?: string
   password?: string
 }
+
+export type Account = {
+  token: string
+}
---
+++ src/tests/signin/index.spec.tsx
@@ -0,0 +1,18 @@
+'use client'
+import React from 'react'
+import { render } from '@testing-library/react'
+import { SignIn } from '@app/app/(signin)/page'
+
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      prefetch: () => null,
+    }
+  },
+}))
+
+describe('SignIn Module', () => {
+  it('Should render SignIn page correctly', () => {
+    render(<SignIn />)
+  })
+})
