1c003ac757feb6a81c4c106eb85c8ddd829f1c70
Lucas Santos <lucas201273@gmail.com>
2024-02-29 18:39:07 -0300

    refactor: add animation wrapper on forgotpassword page

--- src/modules/forgotpassword/ui/components/Form/index.tsx
+++ src/modules/forgotpassword/ui/components/Form/index.tsx
@@ -6,7 +6,7 @@ import {
 import { InputRoot, ButtonRoot } from '@app/components'
 import { motion, yupResolver, useForm, Link } from '@app/libs'
 import { validators } from '@app/utils'
-import { animations, ShowMessage } from '@app/shared'
+import { animations, AnimationWrapper, ShowMessage } from '@app/shared'
 import { SubmitHandler } from 'react-hook-form'
 import { FiLogIn } from 'react-icons/fi'
 
@@ -32,7 +32,7 @@ export function Form() {
       className="flex flex-col items-center justify-center gap-1"
       onSubmit={handleSubmit(onSubmit)}
     >
-      <motion.div {...animations.moveLeft}>
+      <AnimationWrapper animation={animations.moveLeft}>
         <InputRoot.InputWrapper>
           <InputRoot.InputLabel content="E-mail" />
           <InputRoot.ControllerInput
@@ -44,9 +44,9 @@ export function Form() {
           {validateHookFormField(errors, 'email') &&
             ShowMessage(errors.email.message)}
         </InputRoot.InputWrapper>
-      </motion.div>
+      </AnimationWrapper>
 
-      <motion.div {...animations.showOpacity} className="w-full">
+      <AnimationWrapper animation={animations.showOpacity} className="w-full">
         <ButtonRoot.Button
           type="submit"
           className=" mt-1 bg-primaryYellow cursor-pointer hover:opacity-90 disabled:bg-gray-500 text-white w-full"
@@ -54,16 +54,16 @@ export function Form() {
         >
           <ButtonRoot.ButtonContent>Enviar</ButtonRoot.ButtonContent>
         </ButtonRoot.Button>
-      </motion.div>
+      </AnimationWrapper>
 
-      <motion.div {...animations.moveTop}>
+      <AnimationWrapper animation={animations.moveTop}>
         <Link href="/">
           <span className="text-white flex  items-center justify-center mt-2 gap-x-3 cursor-pointer hover: opacity-80">
             <FiLogIn size={20} />
             Já possui conta.
           </span>
         </Link>
-      </motion.div>
+      </AnimationWrapper>
     </form>
   )
 }
