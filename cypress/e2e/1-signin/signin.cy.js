91bb5d31960215e8ef9b7e91a1272dc79be08e87
Lucas Santos <lucas201273@gmail.com>
2024-02-29 16:18:27 -0300

    feat: add cypress

---
+++ cypress/e2e/1-signin/signin.cy.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe('SignIn Page', () => {
+  it('Should render signin page correcty', () => {
+    cy.visit('http://localhost:3001')
+  })
+})
--- src/utils/validators/index.spec.ts
+++ src/utils/validators/index.spec.ts
@@ -1,7 +1,1 @@
 import validator from './index'
-
-describe('Validatior', () => {
-  test('any_test', () => {
-    expect(true).toBe(true)
-  })
-})
